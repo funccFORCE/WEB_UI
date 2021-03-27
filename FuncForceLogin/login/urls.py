acb304c650dc2f7a7fd15733e94a17827cf493eb
Radhika Agrawal <74089267+radhika2352@users.noreply.github.com>
2021-03-27 13:46:02 +0530

    Update urls.py

--- FuncForceLogin/login/urls.py
+++ FuncForceLogin/login/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name="Home"),
     path('login/', views.logincheck, name="login"),
     path('logout/', views.logoutcheck, name="login"),
+    path('signup/', views.signup, name="signup"),
 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
